217d23821db4e7ba7cb1cd590a7e0681c2729349
Benjamin Amos <benjamin.amos11@gmail.com>
2017-12-28 11:12:24 +0000

    Added an untested UI for the deskbar replicant. This does not fix the deskbar replicant not showing.

--- Source/WeatherDeskbarView.h
+++ Source/WeatherDeskbarView.h
@@ -13,7 +13,9 @@ class WeatherDeskbarView : public BView
 {
 public:
 	WeatherDeskbarView(BRect viewSize, BBitmap* weatherIcon);
-	virtual void OnMouseUp();
+	virtual void Pulse();
+	virtual void OnMouseUp(BPoint point);
+	virtual void OnMouseMove(BPoint point);
 	virtual void Draw(BRect drawRect);
 	void SetWeatherIcon(BBitmap* newIcon);
 	virtual status_t	Archive(BMessage* into, bool deep = true) const;
