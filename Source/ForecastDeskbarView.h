c15a4f178c2f484f24c1c871815ec2c4d0ad370d
Benjamin Amos <benjamin.amos11@gmail.com>
2017-12-30 13:03:37 +0000

    Changed the way that the tooltip delay is timed. The changes are untested due to a bug in the creation of the deskbar replicant.

--- Source/ForecastDeskbarView.h
+++ Source/ForecastDeskbarView.h
@@ -8,21 +8,27 @@
 #include <SupportDefs.h>
 #include <View.h>
 #include <Bitmap.h>
+#include <MessageRunner.h>
+#include <Looper.h>
 
-class ForecastDeskbarView : public BView
+class ForecastDeskbarView : public BView, private BLooper
 {
 public:
 	ForecastDeskbarView(BRect viewSize, BBitmap* weatherIcon);
 	ForecastDeskbarView(BMessage* archive);
-	virtual void Pulse();
+	~ForecastDeskbarView();
+	virtual void AttachedToWindow();
 	virtual void OnMouseUp(BPoint point);
 	virtual void OnMouseMove(BPoint point);
 	virtual void Draw(BRect drawRect);
+	virtual void MessageReceived(BMessage* message);
 	void SetWeatherIcon(BBitmap* newIcon);
 	virtual status_t	Archive(BMessage* into, bool deep = true) const;
 	static BArchivable*	Instantiate(BMessage* archive);
 private:
 	BBitmap* fWeatherIcon;
+	BMessageRunner* fMessageRunner;
+	bool fSendToolTip;
 };
 
 
